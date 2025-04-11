cfc6b556fffc5fdbcdaa2d69bb6f23b307aeaea6
Akshay Jain <akshayush007@gmail.com>
2025-04-11 14:12:24 +0530

    get accounts

--- main.py
+++ main.py
@@ -18,12 +18,8 @@ class CustomerIDRequest(BaseModel):
 def read_root():
     return {"message": "Hello, Railway!"}
 
-@app.post("/accounts")
-def get_accounts():
-    return {"message": "Account created successfully"}
-
-@app.post("/accounts/list")
-def get_account_list(request: CustomerIDRequest):
+@app.post("/get_accounts/")
+def get_accounts(request: CustomerIDRequest):
     customer_id = request.customer_id
     if customer_id not in accounts_data:
         raise HTTPException(status_code=404, detail="Customer ID not found")
